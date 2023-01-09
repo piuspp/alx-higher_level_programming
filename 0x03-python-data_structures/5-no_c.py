3867d73fd44066fc5163e6dcabf47bf3e0c0fa3d
piuspp <64987386+piuspp@users.noreply.github.com>
2023-01-09 17:32:38 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,8 +4,4 @@ def no_c(my_string):
     for c in my_string:
         if c.lower() != "c":
             new_string += c
-<<<<<<< HEAD
     return new_string
-=======
-    return 
->>>>>>> 77416e42d8d02f1f2fd1725d4b7faef550b54279
