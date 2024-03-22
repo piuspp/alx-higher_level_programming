0deb941fb4102937f474d35ccd86e09cd993136a
piuspp <go4pius@yahoo.com>
2024-03-22 15:27:40 -0700

    More dat structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
-        if i == search:
+        if my_list[i] == search:
             new_list.append(replace)
         else:
-            new_list.append(i):
+            new_list.append(my_list[i]):
     print(new_list)
