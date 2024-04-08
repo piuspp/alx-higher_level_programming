223e52193c4c12c1f842dcad7258ecaa72605e91
piuspp <go4pius@yahoo.com>
2024-04-08 03:50:26 -0700

    More Date

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-def search_replace(my_list, search, replace):
-        ''' A function that searches and replaces all 
-    occurrences of an element'''
+def search_replace(my_list,search, replace):
+    '''funtion that finds and
+    replaces an element in a list'''
     new_list = []
-    for i in range(0, len(my_list)):
+    for i in range (0, len(my_list)):
         if my_list[i] == search:
             new_list.append(replace)
         else:
             new_list.append(my_list[i])
     return (new_list)
+
