51126d87aa70235d6227e8282d4897f18405ddcd
piuspp <go4pius@yahoo.com>
2024-04-18 14:33:47 -0700

    Classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ An empty class that defines a square"""
 
-Square = __import__('0-square').Square
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+class Square:
+    """a class that define square"""
+    pass
