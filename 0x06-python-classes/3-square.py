72cbca3ac0b4e77810bcfd031ec556ebfbabf29d
piuspp <go4pius@yahoo.com>
2024-04-19 09:00:28 -0700

    Classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
 """Class that defines a square"""
 
-
 class Square:
-    """The class that create sqaure"""
+    """The class to create square."""
 
-    def __init__ (self, size=0):
-        """
-        create an instance
+    def __init__(self, size=0):
+        """set size to private instance variable
 
         Args:
-            size(int):the size of the square
+            size (int): the size of the square
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """Returns square of current area
+        """get the area of the square
 
         Args:
             None
-        
+
         Returns:
             Area of the square(int)
         """
-        return(self.__size*self.__size)
-
-
+        return (self.__size * self.__size)
